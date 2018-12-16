8ac44b74df12309c0fd0e922068d31c71d820d1e
Josef Hornych <hornych@engeto.com>
2018-12-16 23:27:49 +0100

    build and rename babel plugin file

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,79 @@
 import typescript from 'rollup-plugin-typescript2'
 import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
-// import postcss from 'rollup-plugin-postcss-modules'
-import postcss from 'rollup-plugin-postcss'
 import resolve from 'rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
 import svgr from '@svgr/rollup'
 
 import pkg from './package.json'
 
-export default {
-  input: 'src/index.tsx',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      exports: 'named',
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    external(),
-    postcss({
-      modules: true
-    }),
-    url(),
-    svgr(),
-    resolve(),
-    typescript({
-      rollupCommonJSResolveHack: true,
-      clean: true
-    }),
-    commonjs()
-  ]
-}
+export default [
+  {
+    input: 'src/index.tsx',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+        exports: 'named',
+        sourcemap: true
+      },
+    ],
+    plugins: [
+      external(),
+      url(),
+      svgr(),
+      resolve(),
+      typescript({
+        rollupCommonJSResolveHack: true,
+        clean: true
+      }),
+      commonjs()
+    ]
+  },
+  {
+    input: 'src/babel/index.js',
+    output: [
+      {
+        file: 'dist/babel/index.js',
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true
+      },
+      {
+        file: 'dist/babel/index.es.js',
+        format: 'es',
+        exports: 'named',
+        sourcemap: true
+      },
+      { // For development purposes (also mentioned in .gitignore)
+        file: 'babel.js',
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true
+      },
+      { // For development purposes (also mentioned in .gitignore)
+        file: 'babel.es.js',
+        format: 'es',
+        exports: 'named',
+        sourcemap: true
+      },
+    ],
+    plugins: [
+      external(),
+      url(),
+      resolve(),
+      typescript({
+        rollupCommonJSResolveHack: true,
+        clean: true,
+        tsconfigOverrides: { declaration: false }
+      }),
+      commonjs()
+    ]
+  }
+]
