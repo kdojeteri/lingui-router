2aa83d31b9d179ffd0e78c66f07e2bb4e1ca9677
Josef Hornych <hornych@engeto.com>
2019-01-10 15:27:38 +0100

    upgrade version

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
 export {LinguiRouter, WithLinguiRouter, I18nPath, RouterI18n} from './LinguiRouter';
 export {Switch, Route} from './RouteComponents';
 export {NavLinkProps, LinkProps, RedirectProps, Redirect, NavLink, Link} from './LinkComponents';
-export {i18nPath, i18nTo} from './templateTags'
