e692b933accd05e5b79c902bd6c6c4f1953ccd92
Josef Hornych <hornych@engeto.com>
2019-01-14 14:22:15 +0100

    extract and translate routes in example

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react'
 import {Route as RRRoute, Router} from "react-router";
-import {i18nPath, i18nTo, LinguiRouter, NavLink, Route} from "lingui-router";
+import {LinguiRouter, NavLink, Route} from "lingui-router";
 import {I18nProvider, Trans} from "@lingui/react";
 import {createBrowserHistory} from "history";
 import cs from './locale/cs/messages';
 import en from './locale/en/messages';
 import es from './locale/es/messages';
 import fr from './locale/fr/messages';
+
+import csRoutes from './locale/cs/routes';
+import enRoutes from './locale/en/routes';
+import esRoutes from './locale/es/routes';
+import frRoutes from './locale/fr/routes';
 
 
 export default class App extends Component {
@@ -15,8 +20,8 @@ export default class App extends Component {
       <Router history={createBrowserHistory()}>
         <RRRoute path="/:locale?">{({match: {params: {locale}}}) => (
           <I18nProvider language={locale || 'cs'} catalogs={{cs, en, es, fr}}>
-            <LinguiRouter>
-              <h1><Trans>Welcome to <code>lingui-router</code>!</Trans></h1>
+            <LinguiRouter catalogs={{cs: csRoutes, en: enRoutes, es: esRoutes, fr: frRoutes}}>
+              <h1>< Trans> Welcome to <code>lingui-router</code>!</Trans></h1>
               <nav>
                 <ul>
                   <li><NavLink to={"/"}><Trans>Homepage</Trans></NavLink></li>
